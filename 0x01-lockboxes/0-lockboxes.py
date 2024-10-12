1d00de0f5336ae39214b3e95d5149c6b33b556fa
Ohuka Miracle Chigozie <123806072+Miralex20@users.noreply.github.com>
2024-10-12 22:51:00 +0100

    Lockboxes task

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" An Alx interview task """
+from typing import List
+
+
+def canUnlockAll(boxes: List[List[int]]) -> bool:
+    """ A function that performs the
+    locked boxes task"""
+    n = len(boxes)
+    unlocked = set([0])
+    keys = list(boxes[0])
+
+    while keys:
+        key = keys.pop()
+        if key <= n and key not in unlocked:
+            unlocked.add(key)
+            keys.extend(boxes[key])
+
+    return len(unlocked) == n
